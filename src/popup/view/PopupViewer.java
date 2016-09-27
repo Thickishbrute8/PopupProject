2442d1d545009282e99cc646d3539c4f1f7e3147
Britton Perry <bper5656@740s-pl-132.local>
2016-09-27 08:38:32 -0600

    Created an input popup

--- src/popup/view/PopupViewer.java
+++ src/popup/view/PopupViewer.java
@@ -1,6 +1,30 @@
 package popup.view;
 
+import javax.swing.JOptionPane;
+
 public class PopupViewer
 {
 
+	/**
+	 * Makea popup window to display the supplies mesage
+	 * @param message The messagebe placed in the popup
+	 */
+	public void displayMessage(String message)
+	{
+ JOptionPane.showMessageDialog(null,  message);
+	}
+	
+	/**
+	 * Creates an input popup to collect a response from the user as a String.
+	 * @param question the question being asked to the user.
+	 * @return The users response.
+	 */
+	public String collectResponce(String question)
+	{
+		String responce = "";
+		
+		responce = JOptionPane.showInputDialog(null, question);
+		
+		return responce;
+	}
 }
